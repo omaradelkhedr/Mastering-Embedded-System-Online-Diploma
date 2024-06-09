d595d9704f9aa6f15bdf128880c90b44fe6c13c1
Omar Khedr <omaradelkhedr@gmail.com>
2024-06-09 22:10:06 +0300

    Assignment Exercise 7 code

---
+++ Unit 2/Lesson 4/Assignment - Ex 7.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/* Ex 2: Calculate the length of a string manually*/
+int main (void)
+{
+	/* Define a string*/
+	unsigned char text[30];
+	/* Request a string from the user*/
+	printf("Please enter any text: ");
+	gets(text);
+	/* A loop to count characters of a string*/
+	unsigned char count;
+	while (text[count])
+	{
+		/* counter will keep counting until it reaches null character which is a value 0 -> that terminates the loop*/
+		count++;
+	}
+	/* Print the result*/
+	printf("Number of characters in the previous text is : %d",count);
+	return 0;
+}
